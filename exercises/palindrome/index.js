c9e9080b1db2c5f6abf256944d312ffc4eb6ac2a
lisellelogan <liselle.logan@yahoo.com>
2021-09-12 13:34:28 -0400

    add second solution using .reverse()

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,10 +7,16 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-//my solution
+//his solution using .reverse()
 function palindrome(str) {
-    const reversedString = str.split('').reduce((rev, char) => char + rev, '');
-    return reversedString === str;
+    const reversed = str.split('').reverse().join('');
+    return str === reversed;
 }
 
 module.exports = palindrome;
+
+//my solution
+// function palindrome(str) {
+//     const reversedString = str.split('').reduce((rev, char) => char + rev, '');
+//     return reversedString === str;
+// }
