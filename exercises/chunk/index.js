aff29dcd0e219ac86e11f129ccd334f6a71287d0
lisellelogan <liselle.logan@yahoo.com>
2021-10-16 17:16:05 -0400

    solve chunk problem and code alternative solutions

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,37 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+//my solution
+function chunk(array, size) {
+    // we want to take the array and break it into chunks each chunk size equals size of subarray
+    // we probably want to take the array we have and find the index
+    // take size +1 so we can get proper index to slice on
+    let chunksArr = [];
+    let index = 0
+    
+    while(index < array.length) {
+        chunksArr.push(array.slice(index, index + size))
+        index += size
+    }
+    return chunksArr
+}
+
+//his solution
+/*
+function chunk(array, size){
+    const chunked = [];
+
+    for (let element of array) {
+        const last = chunked[chunked.length - 1]
+
+        if (!last || last.length === size) {
+            chunked.push([element])
+        } else {
+            last.push(element);
+        }
+    }
+    return chunked
+}
+ */
 
 module.exports = chunk;
