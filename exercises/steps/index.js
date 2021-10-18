5bd906b55cb31809c76d6d436e9ae386edff3956
lisellelogan <liselle.logan@yahoo.com>
2021-10-18 14:17:02 -0400

    solve steps and write alternatives

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,49 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+    //from 0 to n (iterate through rows)
+    //create an empty string stair
+    //from 0 to n (iterate through columns)
+    //if the current column is equal to or less than the current row
+    // add a # to stair
+    // else
+    // add a space to stair
+    // console log stair
+    for (let row = 0; row < n; row++){
+        let stair = ''
+        for (let col = 0; col < n; col++){
+            if (col <= row){
+                stair += '#'
+            } else {
+                stair += " "
+            }
+        }
+        console.log(stair)
+    }
+}
+
+//recursive solution
+/*
+function steps(n, row = 0, stair = ""){
+    if (n === row) {
+        return;
+    }
+
+    if (n === stair.length) {
+        console.log(stair)
+        return steps(n, row + 1);
+    }
+
+    if (stair.length <= row) {
+        stair += '#';
+    } else {
+        stair += " ";
+    }
+
+    steps(n, row, stair);
+}
+
+*/
 
 module.exports = steps;
